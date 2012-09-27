925bfcb7e05c17f60bc8f935f38a7553150c4cc0
Martin Markech <martin.markech@matho.sk>
2012-09-27 14:07:09 +0200

    we don't need to create photo gallery page on frontend in 0.1.0

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,3 @@ if defined?(::Refinery::User)
   end
 end
 
-if defined?(Refinery::Page) and !Refinery::Page.exists?(:link_url => '/photo-gallery')
-  page = Refinery::Page.create(
-      :title => "Photo galleries",
-      :link_url => "/photo-gallery",
-      :deletable => false,
-      :menu_match => "^/photo-gallery?(\/|\/.+?|)$"
-  )
-
-  Refinery::Pages.default_parts.each do |default_page_part|
-    page.parts.create(:title => default_page_part, :body => nil)
-  end
-end
